7d58ac03810c00713a92d2cab5570cebb87d0dac
Пользователь <ksu1ven@mail.ru>
2023-04-28 20:55:20 +0300

    feat: create keybord without events

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-console.log("мяу");
-import './style.scss'
+import './style.scss';
+import View from './components/view';
+
+class Keyboard {
+  constructor() {
+    this.view = new View();
+    this.lang = localStorage.getItem('a_saved') || 'ru';
+    this.caps = 'off';
+  }
+
+  createKeyboard() {
+    View.createElements();
+    this.view.createLetters(this.lang, this.caps);
+  }
+}
+const keyboard = new Keyboard();
+keyboard.createKeyboard();
